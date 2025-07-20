d863175731d5b036d3d54405ad1f95dba70126f6
shadil-rayyan <shadilrayyan2@gmail.com>
2025-07-21 01:05:38 +0530

    added the cateogroy page to the root layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
+import CategoryPage from './category/page';
 
 const Home = () => {
   return (
-    <div>
-      <h1> welcome type TabLaunch</h1>
-      <p>Launch your favourite website and tool from one place 
-      <Link href="/cateogry"></Link>
+    <div className="text-center">
+      <h1 className="text-4xl font-bold mb-4">Welcome to TabLaunch</h1>
+      <p className="text-lg mb-6">
+        Launch your favorite websites and tools from one place.
       </p>
+      <CategoryPage />
     </div>
-  )
-}
+  );
+};
+
 export default Home;
